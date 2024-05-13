0d201980b7dac07ce2036f18329b42adc72a0b3c
Samantha Kellow <sammii.h@icloud.com>
2024-05-13 17:51:18 +0100

    add config to medua stripe config

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -46,7 +46,11 @@ const plugins = [
   {
     resolve: `medusa-payment-stripe`,
     options: {
-      api_key: process.env.NEXT_PUBLIC_STRIPE_KEY,
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+      capture: true,
+      automatic_payment_methods: true,
+      // payment_description: 
     },
   },
   {
