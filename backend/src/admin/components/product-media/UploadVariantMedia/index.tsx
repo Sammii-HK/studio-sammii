d8d46cb8a0af59837a8a1f8f19d2f7b574a795ca
Samantha Kellow <sammii.h@icloud.com>
2024-05-24 21:21:16 +0100

    UploadVariantMedia component

--- backend/src/admin/components/product-media/UploadVariantMedia/index.tsx
+++ backend/src/admin/components/product-media/UploadVariantMedia/index.tsx
@@ -1,7 +1,95 @@
-const RenameUploadVariantMedia = () => {
+import { useState } from "react"
+import {
+  useAdminCustomPost, 
+  useAdminUploadProtectedFile,
+} from "medusa-react"
+import { 
+  CreateVariantMediaRequest, 
+  CreateVariantMediaResponse, 
+} from "../../../../types/variant-media"
+import { 
+  Button, 
+  Input, 
+  Label, 
+} from "@medusajs/ui"
+import { RouteProps } from "@medusajs/admin-ui"
+
+const UploadVariantMedia = ({
+  notify,
+  productVariantId,
+}: {
+  notify: RouteProps,
+  productVariantId: string
+}) => {
+  // const [name, setName] = useState("")
+  const [file, setFile] = useState<File>()
+  
+  const uploadFile = useAdminUploadProtectedFile()
+  const { 
+    mutate: createVariantMedia,
+    isLoading,
+  } = useAdminCustomPost<
+    CreateVariantMediaRequest,
+    CreateVariantMediaResponse
+  >(
+    "/variant-media",
+    ["variant-media"]
+  )
+
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault()    
+
+    uploadFile.mutate(file, {
+      onSuccess: ({ uploads }) => {
+        if (!("key" in uploads[0])) {
+          return
+        }
+        // create the digital product
+        createVariantMedia({
+          variant_id: productVariantId,
+          file_key: uploads[0].key as string,
+          mime_type: file.type,
+        }, {
+          onSuccess: () => {
+            console.log("success");
+            
+            // notify.success(
+            //   "Success", 
+            //   "Digital Product Created Successfully"
+            // )
+            // navigate("/a/product-media")
+          },
+        })
+      },
+    })
+  }
+
   return (
-    <p>hi</p>
+    <>
+      <form 
+        onSubmit={handleSubmit} 
+        className="flex gap-4 col-auto"
+      >
+        <div className="flex gap-4 items-center">
+          <Input 
+            type="file" 
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+        </div>
+        <Button 
+          variant="primary" 
+          type="submit" 
+          isLoading={
+            uploadFile.isLoading || 
+            isLoading
+          }>
+          Upload
+        </Button>
+      </form>
+    </>
   )
 }
 
-export default RenameUploadVariantMedia;
+export default UploadVariantMedia;
